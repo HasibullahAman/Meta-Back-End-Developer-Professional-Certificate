b3b4cb9730773e470fca2f648df50d9538b07711
Hasibullah Aman <hasib.aman7@gmail.com>
2024-03-27 20:54:34 +0430

    working with module

---
+++ ProgramingWithPython/week4/Module1.py
@@ -0,0 +1,18 @@
+# import sys
+# locatoin = sys.path
+# print(locatoin)
+
+import calendar
+
+# leapdays = calendar.leapdays(2000,2050)
+
+# print(leapdays)
+
+
+# check range of year for leapyear
+
+for i in range(2000,2050):
+    if calendar.isleap(i):
+        print(i)
+    else:
+        continue
