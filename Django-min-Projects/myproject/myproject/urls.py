727f56d3ed04406b279a9202dd1d1c163653ede1
Hasibullah Aman <hasib.aman7@gmail.com>
2024-05-03 18:39:39 +0430

    start mvt

--- Django-min-Projects/myproject/myproject/urls.py
+++ Django-min-Projects/myproject/myproject/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path]
-from myapp.models import views 
+from django.urls import path
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.home,name="home")
+    path('',views.home,name='home'),
 ]
