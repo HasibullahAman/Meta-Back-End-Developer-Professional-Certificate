f7a707ff34fe8925a160cece7012334f1ee39a66
Hasibullah Aman <hasib.aman7@gmail.com>
2024-05-04 10:51:16 +0430

    work with view

--- Django-min-Projects/MVTPractice/View/views.py
+++ Django-min-Projects/MVTPractice/View/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    content = "<html><head><h1>Wellcome to Little Lemon Resturant</h1></html>"
+    return HttpResponse(content)
