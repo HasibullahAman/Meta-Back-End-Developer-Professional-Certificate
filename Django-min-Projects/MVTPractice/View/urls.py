7a3e9504b11088f7a41cf23675539e0c1658bd9a
Hasibullah Aman <hasib.aman7@gmail.com>
2024-05-06 10:51:08 +0430

    httprequest&response

--- Django-min-Projects/MVTPractice/View/urls.py
+++ Django-min-Projects/MVTPractice/View/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path("display_date",views.display_date),
+    
 ]
+
+
--- Django-min-Projects/MVTPractice/View/views.py
+++ Django-min-Projects/MVTPractice/View/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
 
 def home(request):
     content = "<html><head><h1>Wellcome to Little Lemon Resturant</h1></html>"
     return HttpResponse(content)
+
+
+def myview(request):
+    if request.method =='GET':
+        pass
+        # Perform read or delate operation in model
+    if request.method == 'POST':
+        pass
+        # Perform create or update operation in model   
+        context = {}
+    return render(request,'index.html',context)
+
+def  display_date(request):
+    date_joined = datetime.today().year
+    return HttpResponse(date_joined)
+
+
