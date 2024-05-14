61fb46032116de66f58ee5e9384c995800d0f88f
Hasibullah Aman <hasib.aman7@gmail.com>
2024-05-14 10:03:59 +0430

    work with migration

--- Django-min-Projects/Model/menuproject/menuapp/models.py
+++ Django-min-Projects/Model/menuproject/menuapp/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from unicodedata import name
 # Create your models here.
 
-
-class Menu(models.Model):
-    pass
+class Menuitems(models.Model):
+    name = models.CharField(max_length = 100)
+    course = models.CharField(max_length = 100)
+    year = models.IntegerField(default=2000)
+    
+    
+    
+    
+    
