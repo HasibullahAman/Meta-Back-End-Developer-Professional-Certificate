0ec0e1071b7711120ccbc53dce95f918efe06af6
Hasibullah Aman <hasib.aman7@gmail.com>
2024-05-28 17:47:15 +0430

    update in forms

--- Django-min-Projects/form/form/forms.py
+++ Django-min-Projects/form/form/forms.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django import forms
 # Create your views here.
 
+shifts = (
+    
+    ("1" : "Morning"),
+    ("2" : "Afternoon"),
+    ("3" : "Evening"),
+    ("4" : "Night"),
+)
 
-class DemoForms(forms.Form):
-    name = forms.CharField()
+class InputForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    shift = forms.ChoiceField(choices= Shifts)
+    time_log = forms.TimeField()
--- Django-min-Projects/form/forms/models.py
+++ Django-min-Projects/form/forms/models.py
@@ -8,3 +8,14 @@ class Person(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     
+    
+    """
+    Django automatically names the table as appname_modelname, which you can override by assigning the desired name
+    to db_table parameter of the Meta class, to be declared inside the model class.
+    
+    class Student(CommonInfo): 
+    # ... 
+    class Meta(CommonInfo.Meta): 
+        db_table = 'student_info' 
+        
+    """
