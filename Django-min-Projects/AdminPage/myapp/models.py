ad32bbb6b39aaf10a2e3c5fd12d581aef37aaf8a
Hasibullah Aman <hasib.aman7@gmail.com>
2024-06-09 12:06:57 +0430

    start templates

--- Django-min-Projects/AdminPage/myapp/models.py
+++ Django-min-Projects/AdminPage/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=100)
+    contact = models.CharField(max_length=300,blank = True)
+    time = models.TimeField()
+    count = models.IntegerField()
+    notes = models.CharField(max_length = 300, blank = True)
+    
+    
+    
+       
