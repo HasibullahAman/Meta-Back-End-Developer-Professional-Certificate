c1d4c9c1e861a3d4d4b4699f15be4c136cf15d56
Hasibullah Aman <hasib.aman7@gmail.com>
2024-06-11 12:23:49 +0430

    update

--- Django-min-Projects/TemplateLearn/resturant/views.py
+++ Django-min-Projects/TemplateLearn/resturant/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+def about(requst):
+    about_content = {"about": "this is my world, I live alone and I dont have anything in this world yet!, but I have something "} 
+    return render(requst, 'about.html',about_content)
