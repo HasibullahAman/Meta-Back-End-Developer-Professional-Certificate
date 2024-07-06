f29226e677633fc4471ca0f552225b66750066d4
Hasibullah Aman <hasib.aman7@gmail.com>
2024-07-07 00:31:42 +0430

    start api

--- API/FirstAPI/First/models.py
+++ API/FirstAPI/First/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharFeild(max_length = 255)
+    author = models.CharFeild(max_length = 255)
+    price  = models.models.DecimalField(max_digits=5, decimal_places=2)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['price']),
+        ]
+    
