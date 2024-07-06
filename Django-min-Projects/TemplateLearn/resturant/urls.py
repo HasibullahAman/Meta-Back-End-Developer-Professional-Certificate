c6c76cfad8c336d4c9a4bcd285d829a2dc836509
Hasibullah Aman <hasib.aman7@gmail.com>
2024-07-07 00:00:26 +0430

    start api

--- Django-min-Projects/TemplateLearn/resturant/urls.py
+++ Django-min-Projects/TemplateLearn/resturant/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('about',views.about)
+    path('about',views.about),
+    path('newlist',views.mylist),
+    path('menu_card',views.menu_by_id),
+    path('home',views.home),
+    path('menu',views.menu),
+    
 ]
--- Django-min-Projects/TemplateLearn/resturant/views.py
+++ Django-min-Projects/TemplateLearn/resturant/views.py
@@ -1,8 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Menu
 # Create your views here.
 
 
-def about(requst):
-    about_content = {"about": "this is my world, I live alone and I dont have anything in this world yet!, but I have something "} 
-    return render(requst, 'about.html',about_content)
+# def about(requst):
+#     about_content = {"about": "this is my world, I live alone and I dont have anything in this world yet!, but I have something "} 
+#     return render(requst, 'about.html',about_content)
+
+
+def  mylist(request):
+    mylist_content = {"person" : [
+        {"name": "Ahmad",
+         "age": 20,
+         "job": "student"},
+        {
+            "name": "Ali",
+            "age": 23,
+            "job": "teacher"
+        },
+        {
+            "name": "qasim",
+            "age": 25,
+            "job": "student"
+        },
+        ]}
+    return render(request, 'mylist.html', mylist_content)
+
+
+
+def menu_by_id(request):
+    newMenu = Menu.objects.all()
+    newMenu_dict = {'menu': newMenu}
+    return render(request,'menu_cards.html', newMenu_dict)
+
+
+
+def home(request):
+    return render(request, 'home.html')
+def about(request):
+    return render(request, 'about.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+
+
+
+
