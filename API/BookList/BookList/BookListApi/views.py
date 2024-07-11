725006082ded130d6a3e7bbe688caa9ca08dd206
Hasibullah Aman <hasib.aman7@gmail.com>
2024-07-11 21:53:41 +0430

    work on rest api

--- API/BookList/BookList/BookListApi/views.py
+++ API/BookList/BookList/BookListApi/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+
+@api_view()
+def books(request):
+    return Response("List of book",status = status.HTTP_200_ok)
+
 
 # Create your views here.
